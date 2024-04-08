d7d55933a7cdfa79a43b491b7deaf1eaaf957182
Allison <allisonabinger@gmail.com>
2024-04-08 15:24:05 -0500

    a - 04/08 03:23 PM - Writing ajax request under testing

---
+++ ajax/utils.js
@@ -0,0 +1,11 @@
+
+// Request to API
+
+$(document).ready(function() {
+
+	// Using Oklahoma Plants for testing
+	$.ajax({
+		url: 'https://trefle.io/api/v1/distributions/okl/plants?token=SbeeVCPUVgWafdQTER04QdFsOCxJZ1B9WRqtInHqsDY'
+	})
+
+})

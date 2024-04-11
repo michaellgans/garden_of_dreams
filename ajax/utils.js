3a8def646b912db8303f8ee60c9c35cd50759bdb
Allison <allisonabinger@gmail.com>
2024-04-11 16:24:56 -0500

    A - 04/11 04:24 PM - Found API, testing html loading properties

--- ajax/utils.js
+++ ajax/utils.js
@@ -12,43 +12,56 @@
           
 
 
-async function fetchDataAndRender() {
-  try {
-    const response = await fetch('https://trefle.io/api/v1/distributions/okl/plants?token=SbeeVCPUVgWafdQTER04QdFsOCxJZ1B9WRqtInHqsDY');
-    const json = await response.json();
-		console.log('response gathered');
-    console.log(json); // Call the render function with the fetched data
-  } catch (error) {
-    console.error('Error fetching data:', error);
-  }
-}
+// async function fetchDataAndRender() {
+//   try {
+//     const response = await fetch('https://trefle.io/api/v1/distributions/okl/plants?token=SbeeVCPUVgWafdQTER04QdFsOCxJZ1B9WRqtInHqsDY');
+//     const json = await response.json();
+// 		console.log('response gathered');
+//     console.log(json); // Call the render function with the fetched data
+//   } catch (error) {
+//     console.error('Error fetching data:', error);
+//   }
+// }
 
-function renderData(data) {
-	const plantList = document.getElementById('plantContainer');
-	data.forEach(plant => {
-		const plantDiv = document.createElement('div')
-		plantDiv.textContent = plant.common_name;
-		plantList.appendChild(plantDiv);
-	});
-}
+// function renderData(data) {
+// 	const plantList = document.getElementById('plantContainer');
+// 	data.forEach(plant => {
+// 		const plantDiv = document.createElement('div')
+// 		plantDiv.textContent = plant.common_name;
+// 		plantList.appendChild(plantDiv);
+// 	});
+// }
 
-document.addEventListener('DOMContentLoaded', () => {
-	fetchDataAndRender();
-})
-
-
+// document.addEventListener('DOMContentLoaded', () => {
+// 	fetchDataAndRender();
+// })
 
 
 
 
-// $(document).ready(function(){
+
+
+const settings = {
+	async: true,
+	crossDomain: true,
+	url: 'https://plants10.p.rapidapi.com/plants/states?fips_code=US41',
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '213d0efc24msh1e16df4517dc754p1042d8jsna8c568f82bde',
+		'X-RapidAPI-Host': 'plants10.p.rapidapi.com'
+	}
+};
+
+$.ajax(settings).done(function (response) {
+	displayPlantData(response);
+});
 
 // 	// Using Oklahoma Plants for testing
 // 	$.ajax({
-// 		url: 'https://trefle.io/api/v1/distributions/okl/plants?token=SbeeVCPUVgWafdQTER04QdFsOCxJZ1B9WRqtInHqsDY',
+// 		url: 'https://cors-anywhere.herokuapp.com/https://trefle.io/api/v1/distributions/okl/plants?token=SbeeVCPUVgWafdQTER04QdFsOCxJZ1B9WRqtInHqsDY',
 // 		method: 'GET',
 // 		success: function(response) {
-// 			console.log('Successful API response')
+// 			console.log('Successful API response');
 // 			displayPlantData(response.data);
 // 		},
 // 		error: function(xhr, status, error) {
@@ -57,16 +70,15 @@ document.addEventListener('DOMContentLoaded', () => {
 // 	});
 // });
 
-// function displayPlantData(plantData) {
-// 	var plantList = $('#plantContainer');
-// 	plantData.forEach(function(plant) {
-// 		var plantElement = `
-// 		<div class="plant">
-// 			<h2>${plant.common_name}</h2>
-// 			<img src="${plant.image_url}" alt="${plant.common_name}">
-// 			<p>${plant.scientific_name}</p>
-// 		</div>
-// 		`;
-// 		plantList.append(plantElement)
-// 	});
-// }
+function displayPlantData(plantData) {
+	var plantList = $('#plantContainer');
+	plantData.data.forEach(function(plant) {
+		var plantElement = `
+		<div class="plant">
+			<h2>${plant.common_name}</h2>
+			<p>${plant.scientific_name}</p>
+		</div>
+		`;
+		plantList.append(plantElement);
+	});
+}
